0f54ed026826e4a334d040f30ee9065098882c11
ahstern100 <ahstern100@gmail.com>
2026-04-14 16:50:00 +0000

    Initial commit

---
+++ print.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        extend: {
+            "colors": {
+                "primary": "#0058bc",
+                "secondary": "#a53500",
+                "tertiary": "#765600",
+                "outline": "#558400",
+                "surface-container": "#f8fafc"
+            },
+            "borderRadius": {
+                "DEFAULT": "1rem",
+                "lg": "2rem",
+                "xl": "3rem",
+                "full": "9999px"
+            },
+            "fontFamily": {
+                "headline": ["Heebo", "sans-serif"],
+                "body": ["Assistant", "sans-serif"]
+            }
+        },
+    },
+}
